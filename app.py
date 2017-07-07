8953fa59c250b644245425ced1108f6b39d7a375
Casey M <caseym@gmail.com>
2017-07-07 14:39:22 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+import requests
+
+from models import Post, Session
+
+
+def main():
+    session = Session()
+    items = get_items()
+
+    if items:
+        delete_items(session)
+        save_items(items, session)
+    session.commit()
+    session.close()
+
+
+def get_items():
+    """Get posts from API."""
+    endpoint = 'https://jsonplaceholder.typicode.com/posts'
+
+    r = requests.get(endpoint)
+    items = r.json()
+    return items
+
+
+def save_items(items, session):
+    """Save posts to DB."""
+    for item in items:
+        post = Post()
+        post.id = item['id']
+        post.userId = item['userId']
+        post.title = item['title']
+        post.body = item['body']
+        session.add(post)
+
+def delete_items(session):
+    """Delete all posts."""
+    session.query(Post).delete(synchronize_session=False)
+
+
+if __name__ == "__main__":
+    main()
---
+++ models.py
@@ -0,0 +1,25 @@
+import os
+from sqlalchemy import create_engine, Column, DateTime, func, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# database configuration
+engine = create_engine(os.environ['DATABASE_URL'])
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+
+
+# models
+class Post(Base):
+    """Post model."""
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True)
+    userId = Column(Integer)
+    title = Column(String(200))
+    body = Column(String)
+    last_updated = Column(DateTime, server_default=func.now())
+
+
+# create tables
+Base.metadata.create_all(engine)
